507af39e913fa6dd23076c620f715de563c1d9c5
Masina Datta Sai <dattasaimasina@gmail.com>
2025-10-13 20:42:33 +0530

    updated program to print 100 numbers

--- src/java2025/FirstHundredNumbers.java
+++ src/java2025/FirstHundredNumbers.java
@@ -3,7 +3,9 @@ package java2025;
 public class FirstHundredNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i =1; i <=100; i++) {
+			System.out.println(i);
+		}
 
 	}
 
