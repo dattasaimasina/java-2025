e3eea9271e3537e028d02ce9aca6e31439cb6567
Masina Datta Sai <dattasaimasina@gmail.com>
2025-10-17 21:18:23 +0530

    updated code

--- src/java2025/FirstHundredNumbers.java
+++ src/java2025/FirstHundredNumbers.java
@@ -1,5 +1,5 @@
 package java2025;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class FirstHundredNumbers {
 
@@ -37,29 +37,80 @@ public class FirstHundredNumbers {
 //	System.out.println("count of evenNumbers" + evenCount);
 //	System.out.println("count of oddNumbers" + oddCount);
 		
-		  Scanner sc = new Scanner(System.in);
-	        
-	        System.out.print("Enter a number: ");
-	        int n = sc.nextInt();  // Read user input
-	        
-	        long product = 1;  // Start with 1 because multiplying by 0 gives 0
-	        
-	        for (int i = 1; i <= n; i++) {
-	            product *= i;  // Multiply each number from 1 to n
-	        }
-	        
-	        System.out.println("Product of numbers from 1 to " + n + " is: " + product);
-	        
-	        sc.close();
+//		  Scanner sc = new Scanner (System.in);
+//	        
+//	        System.out.print("Enter a number: ");
+//	        int n = sc.nextInt();  
+//	        
+//	        long product = 1;  
+//	        
+//	        for (int i = 1; i <= n; i++) {
+//	            product *= i; 
+//	        }
+//	        
+//	        System.out.println("Product of numbers from 1 to " + n + " is: " + product);
+//	        
+//	        sc.close();
 		
-	
-	
-       
-
-//			}
+//		Using Break in a Loop
 		
+//		for (int i = 1; i<=5; i++) {
+//		if (i == 4) {
+//			break;
 //		}
+//			System.out.println(i);
+		
+		
+		
+//		Using continue in a Loop
+		
+		for (int i = 1; i<=10; i++) {
+			
+		
+			System.out.println(i);
+			System.out.println(i);
+			if (i == 6) {
+				continue;
+			}
+			System.out.println(i);
+	
+			
+			
+//		FOR-EACH LOOP
+//		int[] numbers = {10,20,30,40,50,60};
+//		for (int num : numbers) {
+//			System.out.println(num);
+		
+//		PRINT EVEN NUMBERS USING FOR IF
+		
+//		for (int i=1; i<=100; i++) {
+//		if (i % 2 == 0) {
+//			System.out.println(i);
+		
+		
+//	PRINT MULTIPLES OF 3 OR 5
+		
+//		for (int i=1; i<=30; i++) {
+//			if (i % 3 ==0) {
+//				System.out.println(i + "");
+		
+//				for (int i = 1; i<=50; i++) {
+//					if (i % 3 ==0 || i % 5 == 0) {
+//						System.out.println(i + " ");
+//		int n = 4;
+//		for (int i = 1; i<=n; i++) {
+//			for (int j = 1; j<=i; j++) {
+//				System.out.println("*");
+			
+			
+			
+					
+			
+		
+		
+		
+			}
+		}
 
 	}
-
-}
+//}
