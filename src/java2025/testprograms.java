0a9155da4c823fc3b2821cf968be3ba2c3ab0cde
Masina Datta Sai <dattasaimasina@gmail.com>
2025-10-27 22:44:20 +0530

    updated code

---
+++ src/java2025/testprograms.java
@@ -0,0 +1,87 @@
+package java2025;
+import java.util.Scanner;
+public class testprograms {
+
+	public static void main(String[] args) {
+//		int[] numbers = {10,20,30,40,50};
+//		int sum = 0;
+//		for (int i =0; i < numbers.length; i++) {
+//			sum = sum + numbers[i];
+//		}
+//		System.out.println(sum);
+		
+		
+		
+//		COUNT EVEN AND ODD
+		
+//		int[] numbers = {3,6,9,12,15};
+//		int evenCount = 0;
+//		int oddCount = 0;
+//		for (int i : numbers) {
+//			if (i % 2 ==0) {
+//				evenCount++;
+//				}
+//			else {oddCount++;
+//			}
+//		}
+//				System.out.println("count of odd numbers is " + oddCount);
+//				System.out.println("count of even numbers is "+evenCount);
+		
+		
+		
+//		int[] numbers = {5,8,12,3,9};
+//		int largest = 0;
+//		for (int num : numbers ) {
+//				if (num > largest) {
+//					largest = num;
+//				}
+//		}
+//					System.out.println(largest);
+		
+		
+		
+		
+//		int[] numbers = {1,2,3,4,5};
+//		for (int i = numbers.length; i>0; i--) {
+//			System.out.println(i);
+		
+		
+		
+//		int[] numbers = {4,7,1,9,2};
+//		Scanner sc = new Scanner (System.in);
+//		int num = sc.nextInt();
+//		boolean found = false;
+//		for (int i = 0; i< numbers.length; i++) {
+//			if (numbers[i] == num) {
+//				found = true;
+//				break;
+//			}
+//		}
+//		if (found) {
+//			System.out.println(num + " - is exists in the array");
+//		}else {
+//			System.out.println(num + " - does not exits in the array");
+//		}
+//	
+//		sc.close();
+		
+		
+		
+		
+//		int[] numbers = {5,9,2,11,7};
+//		for (int i = 0; i< numbers.length; i++) {
+//			System.out.println(numbers[i]);
+		
+		
+		
+		int[] numbers = {5,9,2,11,7};
+		int largest = 0;
+		for (int num : numbers) {
+			if (num > largest) {
+				largest = num;
+			}
+		}
+				System.out.println(largest);
+			
+	}
+}
